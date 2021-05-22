fb2247d4171ce091e9e89fdbaa4bcdc264af4dbb
CheukHinHo <cheukhinho@gmail.com>
2021-05-22 23:50:35 +0800

    update finite difference for fine grid, and plan to implement GD algorithm

--- UROP/main.py
+++ UROP/main.py
@@ -2,8 +2,8 @@ import numpy as np
 import sympy as sp
 from scipy.optimize import fsolve
 
-a = 1
-b = 1
+a = -2
+b = -3
 
 # range of x: [-1,1], periodic boundary condition is assumed.
 # problem : u*u_x-u_xx=0, u(-1)=a, u(1)=b
@@ -23,17 +23,18 @@ Dx2[0, :] = np.zeros(N)
 Dx2[N - 1, :] = np.zeros(N)
 Dx2[0, 0] = -1
 Dx2[N - 1, N - 1] = -1
-b = np.zeros(N - 2)
-b = np.hstack([a, N - 2, b])
+
+#initializing a vector for implementing boundary condition
+r = np.hstack([a, np.zeros(N - 2), b])
 
 
-def func(u):
-    return u * (Dx1 @ u) - Dx2 @ u - b
+def CoarseFunc(u):
+    return u * (Dx1 @ u) - Dx2 @ u - r
 
 
-print(func(10 * np.ones(10)))
-sol = fsolve(func, np.array([10, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
-print(sol)
+coarseSol = fsolve(CoarseFunc, np.array([10, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+print(coarseSol)
+print(CoarseFunc(coarseSol))
 
 # Next for each of the [u(x_j),u(x_j+1)], we need to apply finite difference method again to solve the solution locally
 #  and aim at obtaining training data for the NN, such that for each input: (u(i),u(i+1)) on the coarse grid, we
@@ -42,3 +43,34 @@ print(sol)
 # Consider the problem on the subinterval [x_j,x_j+1] on [-1,1] such that :
 # phi_j*phi_j_x-phi_j_xx=0, phi_j(x_j)=u(xj), u(x_j+1)=u(x_j+1). We hope to obtain such phi_j and approximate the
 # derivative of phi_j at x_j and x_j+1 respectively, and this is 2 of the target output for the NN.
+
+# consider the local problem on [xj, xj+1]: y*y_x-y_xx=0; y(xj)=u(xj); y(xj+1)=u(j+1).
+# Similar to the above, use finite difference scheme with N2=10
+# after this step, we expect to have y0,y2, ..., yN-1, where yi is the solution on [xi,xi+1]
+N2 = 10
+h2 = 2 / N2
+
+#define FineFunc that value of r2 changes each loop in order to apply the boundary condition
+
+def FineFunc(u):
+    return u * (Dx1 @ u) - Dx2 @ u - r2
+
+
+for i in range(1):
+    r2 = np.hstack([coarseSol[i], np.zeros(N-2), coarseSol[i + 1]])
+    fineSol = fsolve(FineFunc, np.array([10, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+
+print(fineSol)
+print(FineFunc(fineSol))
+
+
+###################################Steepest gradient descent algorithm ###############################################################
+
+#Consider the objective function F(u1,u2,....,uN), where F is defined as:
+# sum_i=1^N(partial y/partial x (ui-1)-partial y/partial x (ui))^2
+# we want to find the minimizer of F, namely u*.
+#what we need to do is to compute the value of 5 derivative by NN at each iteration, where the Gradient
+#descent scheme is given by u(k+1)=u(k)-grad(F), i.e. for each i, ui(k+1)=ui(k)-partial(F)/partial(ui)
+
+
+
