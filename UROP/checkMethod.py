e0cb35ff384337d859da6ad71ffb1ed2ad1ec6be
CheukHinHo <cheukhinho@gmail.com>
2021-06-13 01:01:56 +0800

    - updating the gradientdescent.py algorithm
    - added the target function F
    
    - add different testing set for gradient descent

--- UROP/checkMethod.py
+++ UROP/checkMethod.py
@@ -17,8 +17,8 @@ model = load_model('model/model_10intervals.h5')
 
 N = 10
 # boundary condition for the target problem, u(-1)=a, u(1)=b
-a = -0.3
-b = 0.3
+a = 0.8
+b = 0.2
 r0 = np.hstack([a, np.zeros(N * N - 1), b])
 # step size on fine grid
 h = (2 / N) / N
@@ -51,21 +51,22 @@ for i in range(1, N + 1):
 print("The coarse sol is", real_coarse_sol)
 print("The error vector is: ", coarseFunc(sol))
 
-# checking the input from NN
-diff_sol = np.zeros([2, N])
-for i in range(N - 1):
-    diff_sol[:, i] = np.array(
-        [(sol[(i * N + 1)] - sol[(i * N)]) / h, (sol[((i + 1) * N)] - sol[(i + 1) * N - 1]) / h])
-    print("The", i, "th diff_sol is: ", diff_sol)
-# diff_sol[:, N-2] = np.array([(sol[N * N - 1] - sol[N * N - 2]) / h,
-#                                                    (sol[N * N - 1] - sol[0]) / h])
-print(diff_sol.shape)
-nn_sol = np.zeros([N, 6])
-for k in range(N):
-    nn_sol[k, :] = model.predict(np.array([[real_coarse_sol[k:k + 2]]]))
-# nn_sol = np.vstack((nn_sol, model.predict(np.array([[real_coarse_sol[N - 1], real_coarse_sol[0]]]))))
-print(nn_sol)
-nn_diff_sol = nn_sol[:, 0:2].T
-print("The solution from nn is", nn_diff_sol)
-print("The original solution is", diff_sol)
-print("The error from the original fine solution to that of the output from NN is:", diff_sol - nn_diff_sol)
+# # checking the input from NN
+# diff_sol = np.zeros([2, N])
+# for i in range(N - 1):
+#     diff_sol[:, i] = np.array(
+#         [(sol[(i * N + 1)] - sol[(i * N)]) / h, (sol[((i + 1) * N)] - sol[(i + 1) * N - 1]) / h])
+#     print("The", i, "th diff_sol is: ", diff_sol)
+# # diff_sol[:, N-2] = np.array([(sol[N * N - 1] - sol[N * N - 2]) / h,
+# #                                                    (sol[N * N - 1] - sol[0]) / h])
+# print(diff_sol.shape)
+# nn_sol = np.zeros([N, 6])
+# for k in range(N):
+#     nn_sol[k, :] = model.predict(np.array([[real_coarse_sol[k:k + 2]]]))
+# # nn_sol = np.vstack((nn_sol, model.predict(np.array([[real_coarse_sol[N - 1], real_coarse_sol[0]]]))))
+# print(nn_sol)
+# nn_diff_sol = nn_sol[:, 0:2].T
+# print("The solution from nn is", nn_diff_sol)
+# print("The original solution is", diff_sol)
+# print("The error from the original fine solution to that of the output from NN is:", diff_sol - nn_diff_sol)
+
--- UROP/gradientdescent.py
+++ UROP/gradientdescent.py
@@ -26,8 +26,16 @@ def coarseFunc(u):
 
 N = 10
 # boundary condition for the target problem, u(-1)=a, u(1)=b
-a = -0.3
-b = 0.3
+
+a = 0.6
+b = -0.8
+
+# a = -0.3
+# b = 1
+#
+# a = 0.8
+# b = 0.2
+
 r0 = np.hstack([a, np.zeros(N - 1), b])
 # step size on coarse grid
 h = 2 / N
@@ -50,21 +58,26 @@ data_x = np.loadtxt('data_x2_10interval.txt', delimiter=',')
 target = np.loadtxt('target2_10interval.txt', delimiter=',')
 model = load_model('model/model_10intervals.h5')
 N = 10
-sol = np.array([-0.5, -0.38115072, -0.29389888, -0.22509515, -0.16771603, -0.1175893,
-                -0.0719902, -0.02895759, 0.01308412, 0.05557068, 0.1])
+
+sol = np.array([0.6, 0.44647016, 0.22890003, -0.03090544, -0.28457717, -0.48797398,
+                -0.62682007, -0.71150875, -0.7596639, -0.78595875, -0.8])
+# sol = np.array([-0.3, -0.19805781, -0.11122511, -0.03186265, 0.04548198, 0.12572309,
+#                 0.21455435, 0.32025255, 0.45737722, 0.65668735, 1.])
+# sol = np.array([0.8, 0.79858638, 0.79590838, 0.79084667, 0.78132081, 0.76353872,
+#                 0.7308424, 0.67236035, 0.57274539, 0.41640301, 0.2])
 
 # initial guess
 u_array = np.empty([N + 1, 1])
 u_iter = np.linspace(a, b, N + 1)[1:-1]
 u = np.hstack([a, u_iter, b])
-#u_iter = sol[1:-1]
+# u_iter = sol[1:-1] + np.random.rand(sol.shape[0] - 2)
 print(coarseFunc(sol))
 
 # instead of defining function F, we set the stopping criteria as |e_k|=|uk+1-u_k| since then we don't need to compute
 # all partial derivative for every loop
 
 count = 0
-alpha = 0.0001
+alpha = 0.001
 tol = 0.0000001
 
 while np.linalg.norm(coarseFunc(u)) > tol:
@@ -79,24 +92,28 @@ while np.linalg.norm(coarseFunc(u)) > tol:
     # using the prediction to do the iteration
     # print(count, "th store", store)
 
+    F = np.linalg.norm(store[0:N - 1, 1] - store[1:N, 0]) ** 2
+    print("The error of F is", F)
+
     # update u[2,N-2],which is the sol except the first two and last two entry
     # the first two corresponding to u[2]
-    grad = 2 * (store[0:N - 3, 1] - store[1:N - 2, 0]) * (store[1:N - 2, 5]) + 2 * (
-                store[1:N - 2, 5] - store[2:N - 1, 2]) * (store[1:N - 2, 1] - store[2:N - 1, 0]) + 2 * (
+    grad = 2 * (store[0:N - 3, 1] - store[1:N - 2, 0]) * (store[1:N - 2, 4]) + 2 * (
+            store[1:N - 2, 5] - store[2:N - 1, 2]) * (store[1:N - 2, 1] - store[2:N - 1, 0]) + 2 * (
                    store[2:N - 1, 1] - store[3:N, 0]) * (store[2:N - 1, 3])
     u_iter[1:-1] = u_iter[1:-1] - alpha * grad
 
-    # updatre the second last and second first entry, i.e. u[1] and u[N-1], where u is the solution
+    # update the second last and second first entry, i.e. u[1] and u[N-1], where u is the solution
     u_iter[0] = u_iter[0] - alpha * (2 * (store[0, 5] - store[1, 2]) * (store[0, 1] - store[1, 0]) + 2 * (
             store[1, 1] - store[2, 0]) * (store[1, 3]))
-    u_iter[-1] = u_iter[-1] - alpha * (2 * (store[N - 3, 1] - store[N - 2, 0]) * (store[N - 2, 5]) + 2 * (
-                store[N - 2, 5] - store[N - 1, 2]) * (store[N - 2, 1] - store[N - 1, 0]))
+    u_iter[-1] = u_iter[-1] - alpha * (2 * (store[N - 3, 1] - store[N - 2, 0]) * (store[N - 2, 4]) + 2 * (
+            store[N - 2, 5] - store[N - 1, 2]) * (store[N - 2, 1] - store[N - 1, 0]))
 
     u = np.hstack([a, u_iter, b])
     u_array = np.append(u_array, u)
+    print(count)
     # print(u_iter)
-    # print("Error for each entry when plugging in into original discretized system :",
-    # np.linalg.norm(coarseFunc(u)) / N)
+    print("Error for each entry when plugging in into original discretized system :", np.linalg.norm(coarseFunc(u)))
+    # print("Error array: ", u - sol)
     print("Error with fine grid:", np.linalg.norm(u - sol) / np.linalg.norm(sol))
 
 # print(count)
