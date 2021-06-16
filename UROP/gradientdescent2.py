4e959b4f70ab3635866a66fceb2a461be7f39d05
CheukHinHo <cheukhinho@gmail.com>
2021-06-16 12:35:45 +0800

    added few files ends with "2", which corresponding to 10*100 intervals on fine grid

--- UROP/gradientdescent2.py
+++ UROP/gradientdescent2.py
@@ -17,8 +17,8 @@ def udiff(saveArray):
     return np.linalg.norm(saveArray[-1] - saveArray[-2])
 
 
-def coarseFunc(u):
-    return u * (Dx1 @ u) - Dx2 @ u - r0
+# def coarseFunc(u):
+#     return u * (Dx1 @ u) - Dx2 @ u - r0
 
 
 # defining target function, take DU as an input of size (which contains different partial derivative from the NN
@@ -36,42 +36,18 @@ b = -0.8
 # a = 0.8
 # b = 0.2
 
-r0 = np.hstack([a, np.zeros(N - 1), b])
-# step size on coarse grid
-h = 2 / N
-# assigning derivative matrix
-Dx1 = np.eye(N + 1, k=1) - np.eye(N + 1, k=-1)
-Dx1 = Dx1 / (2 * h)
-# First and last row of Dx1 is zero for applying boundary condition
-Dx1[0, :] = np.zeros(N + 1)
-Dx1[N, :] = np.zeros(N + 1)
-
-Dx2 = np.eye(N + 1, k=1) + np.eye(N + 1, k=-1) - 2 * np.eye(N + 1, k=0)
-Dx2 = Dx2 / (2 * h * 2 * h)
-
-# For applying boundary condition
-Dx2[0, :] = np.hstack((-1, np.zeros(N)))
-Dx2[N, :] = np.hstack((np.zeros(N), -1))
-
 # import model for looping
-data_x = np.loadtxt('data_x2_10interval.txt', delimiter=',')
-target = np.loadtxt('target2_10interval.txt', delimiter=',')
-model = load_model('model/model_10intervals.h5')
-N = 10
+model = load_model('model/model_100*10intervals.h5')
 
-sol = np.array([0.6, 0.44647016, 0.22890003, -0.03090544, -0.28457717, -0.48797398,
-                -0.62682007, -0.71150875, -0.7596639, -0.78595875, -0.8])
-# sol = np.array([-0.3, -0.19805781, -0.11122511, -0.03186265, 0.04548198, 0.12572309,
-#                 0.21455435, 0.32025255, 0.45737722, 0.65668735, 1.])
-# sol = np.array([0.8, 0.79858638, 0.79590838, 0.79084667, 0.78132081, 0.76353872,
-#                 0.7308424, 0.67236035, 0.57274539, 0.41640301, 0.2])
+sol = np.array([0.6, 0.44651126, 0.22901778, -0.0307047, -0.28433673, -0.48775492,
+                -0.62665791, -0.71140697, -0.75961002, -0.7859378, -0.8])
+
 
 # initial guess
 u_array = np.empty([N + 1, 1])
 u_iter = np.linspace(a, b, N + 1)[1:-1]
 u_iter = np.copy(sol[1:-1])
 u = np.hstack([a, u_iter, b])
-print(coarseFunc(sol))
 
 # instead of defining function F, we set the stopping criteria as |e_k|=|uk+1-u_k| since then we don't need to compute
 # all partial derivative for every loop
@@ -96,12 +72,12 @@ while count < 100:
     print(count)
     print("The error of F of last iteration is: ", F)
     # update u[2,N-2],which is the sol except the first two and last two entry
+    print(store)
     # the first two corresponding to u[2]
     grad = 2 * (store[0:N - 3, 1] - store[1:N - 2, 0]) * (store[1:N - 2, 4]) + 2 * (
             store[1:N - 2, 5] - store[2:N - 1, 2]) * (store[1:N - 2, 1] - store[2:N - 1, 0]) + 2 * (
                    store[2:N - 1, 1] - store[3:N, 0]) * (store[2:N - 1, 3])
     u_iter[1:-1] = u_iter[1:-1] - alpha * grad
-    print(store)
     # update the second last and second first entry, i.e. u[1] and u[N-1], where u is the solution
     u_iter[0] = u_iter[0] - alpha * (2 * (store[0, 5] - store[1, 2]) * (store[0, 1] - store[1, 0]) + 2 * (
             store[1, 1] - store[2, 0]) * (store[1, 3]))
@@ -117,4 +93,3 @@ while count < 100:
 
 # print(count)
 print("end")
-print(coarseFunc(u))
---
+++ UROP/network2.py
@@ -0,0 +1,75 @@
+import numpy as np
+import tensorflow as tf
+from numpy import linalg
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import regularizers
+
+""" Training of NN  """
+
+data_x = np.loadtxt('data_x_100*10interval.txt', delimiter=',')
+target = np.loadtxt('target_100*10intervals.txt', delimiter=',')
+
+
+def step_decay(epoch):
+    initial_learning_rate = 0.01
+    lrate = initial_learning_rate * 0.96 ** (int((epoch - 1) / 200))
+    return lrate
+
+
+def normalize(X, Y):
+    X = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
+    Y = (Y - np.mean(Y, axis=0)) / np.std(Y, axis=0)
+    return X, Y, np.std(X, axis=0), np.std(Y, axis=0), np.mean(X, axis=0), np.mean(Y, axis=0)
+
+
+def denomalize(prediction, meany, stdy):
+    prediction = prediction * stdy + meany
+    return prediction
+
+
+def calError(prediction, target):
+    error = 0
+    for i in range(len(target)):
+        error = error + np.linalg.norm(prediction[i] - target[i]) / np.linalg.norm(target[i])
+    error = error / len(target)
+    print(error)
+
+
+# print(data_x.shape)
+# print(target.shape)
+# print(np.std(data_x, axis=0))
+# normalized_data_x, normalized_target, std_x, std_y, mean_x, mean_y = normalize(data_x, target)
+train_valid_x, test_x, train_valid_y, test_y = train_test_split(data_x, target, test_size=0.2, random_state=42)
+train_x, valid_x, train_y, valid_y, = train_test_split(train_valid_x, train_valid_y, test_size=0.2, random_state=41)
+
+model = tf.keras.models.Sequential()
+lrate = tf.keras.callbacks.LearningRateScheduler(step_decay)
+
+checkpoint_filepath = 'model/model1'
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_filepath,
+    save_weights_only=True,
+    monitor='val_loss',
+    mode='min',
+    save_best_only=True)
+
+earlystop_callback = tf.keras.callbacks.EarlyStopping(monitor='loss', min_delta=0, patience=6)
+
+model.add(tf.keras.Input(shape=2))
+model.add(tf.keras.layers.Dense(10, activation='relu', activity_regularizer=regularizers.l2(1e-4)))
+model.add(tf.keras.layers.Dense(10, activation='relu', activity_regularizer=regularizers.l2(1e-4)))
+model.add(tf.keras.layers.Dense(10, activation='relu', activity_regularizer=regularizers.l2(1e-4)))
+model.add(tf.keras.layers.Dense(6, activation='linear', activity_regularizer=regularizers.l2(1e-4)))
+
+model.compile(optimizer='adam', loss='mse', metrics=['MeanSquaredError'])
+
+model.fit(train_x, train_y, validation_data=(valid_x, valid_y), epochs=20000, batch_size=1,
+          callbacks=[earlystop_callback, lrate, model_checkpoint_callback])
+
+predictions = model.predict(test_x)
+
+calError(predictions, test_y)
+
+print(model.predict(data_x))
+
+model.save("model_100*10intervals.h5")
